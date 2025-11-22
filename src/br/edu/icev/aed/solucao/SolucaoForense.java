8333cd6ec49dece9a09cc57b33dca9c560d86d72
Serminho <samuelegsonmr@gmail.com>
2025-11-22 02:18:17 -0300

    Primeiro commit

---
+++ src/br/edu/icev/aed/solucao/SolucaoForense.java
@@ -0,0 +1,35 @@
+package br.edu.icev.aed.solucao;
+
+import br.edu.icev.aed.forense.AnaliseForenseAvancada;
+import br.edu.icev.aed.forense.Alerta;
+
+import java.io.IOException;
+import java.util.*;
+
+public class SolucaoForense implements AnaliseForenseAvancada {
+
+    @Override
+    public Set<String> encontrarSessoesInvalidas(String caminhoArquivo) throws IOException {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public List<String> reconstruirLinhaTempo(String caminhoArquivo, String sessionId) throws IOException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Alerta> priorizarAlertas(String caminhoArquivo, int n) throws IOException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Map<Long, Long> encontrarPicosTransferencia(String caminhoArquivo) throws IOException {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Optional<List<String>> rastrearContaminacao(String caminhoArquivo, String recursoInicial, String recursoAlvo) throws IOException {
+        return Optional.empty();
+    }
+}
