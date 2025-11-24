6c22f9d26095b1d1eca1e9d41241fc2d43bb47d1
Absalão <absalaoresplandes@gmail.com>
2025-11-24 20:42:30 -0300

    adicionando-implementação-do-desafio3

--- src/br/edu/icev/aed/forense/SolucaoForense.java
+++ src/br/edu/icev/aed/forense/SolucaoForense.java
@@ -17,8 +17,52 @@ public class SolucaoForense implements AnaliseForenseAvancada {
 
     @Override
     public List<Alerta> priorizarAlertas(String caminhoArquivo, int n) throws IOException {
-        return Collections.emptyList();
+
+        List<Alerta> resultado = new ArrayList<>();
+
+        if (n <= 0) {
+            return resultado;
+        }
+        Comparator<Alerta> comparatorAlertas = Comparator.comparingInt(Alerta::getSeverityLevel).reversed();
+
+        PriorityQueue<Alerta> filaDePrioridade = new PriorityQueue<>(comparatorAlertas);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+
+            String linha;
+
+            while ((linha = br.readLine()) != null) {
+
+                String[] partes = linha.split(";");
+
+                if (partes.length != 7) {
+                    continue; // se a linha for inválida ele ignora
+                }
+
+                long timestamp = Long.parseLong(partes[0]);
+                String userId = partes[1];
+                String sessionId = partes[2];
+                String actionType = partes[3];
+                String targetResource = partes[4];
+                int severity = Integer.parseInt(partes[5]);
+                long bytesTransferred = Long.parseLong(partes[6]);
+
+                Alerta alerta = new Alerta(timestamp, userId, sessionId, actionType, targetResource, severity, bytesTransferred);
+                filaDePrioridade.add(alerta);
+            }
+        }
+
+        // vai Extrair os n primeiros
+        for (int i = 0; i < n && !filaDePrioridade.isEmpty(); i++) {
+
+            resultado.add(filaDePrioridade.poll());
+        }
+
+        return resultado;
     }
+}
+
+
 
     @Override
     public Map<Long, Long> encontrarPicosTransferencia(String caminhoArquivo) throws IOException {
