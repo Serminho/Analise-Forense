a74ecf1d010a662a07c2f6165d4f50698bd4938c
Daadilo <danilocamposfernandes22@gmail.com>
2025-11-24 11:28:04 -0300

    Update SolucaoForense.java

--- src/br/edu/icev/aed/solucao/SolucaoForense.java
+++ src/br/edu/icev/aed/solucao/SolucaoForense.java
@@ -8,16 +8,58 @@ import java.util.*;
 
 public class SolucaoForense implements AnaliseForenseAvancada {
 
-    @Override
-    public Set<String> encontrarSessoesInvalidas(String caminhoArquivo) throws IOException {
-        return Collections.emptySet();
+    private List<String[]> ler(String caminho) throws IOException {
+        var linhas = new ArrayList<String[]>();
+        try (var br = new BufferedReader(new FileReader(caminho))) {
+            br.readLine();
+            String l;
+            while ((l = br.readLine()) != null) {
+                String[] c = l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                for (int i = 0; i < c.length; i++) c[i] = c[i].replace("\"", "").trim();
+                if (c.length >= 6) linhas.add(c);
+            }
+        }
+        return linhas;
+    }
+
+ @Override
+    public Set<String> encontrarSessoesInvalidas(String caminhoArquivoCsv) throws IOException {
+        var logs = ler(caminhoArquivoCsv);
+        var cont = new HashMap<String, Integer>();
+
+        for (String[] log : logs) {
+            String sessao = log[1];
+            String acao   = log[3];
+
+            cont.putIfAbsent(sessao, 0);
+            if ("LOGIN".equals(acao)) {
+                cont.put(sessao, cont.get(sessao) + 1);
+            } else if ("LOGOUT".equals(acao) || "SESSION_EXPIRED".equals(acao)) {
+                int v = cont.get(sessao);
+                if (v > 0) cont.put(sessao, v - 1);
+            }
+        }
+
+        var invalidas = new TreeSet<String>();
+        for (var e : cont.entrySet()) {
+            if (e.getValue() > 0) invalidas.add(e.getKey());
+        }
+        return invalidas;
     }
 
     @Override
-    public List<String> reconstruirLinhaTempo(String caminhoArquivo, String sessionId) throws IOException {
-        return Collections.emptyList();
-    }
+    public List<String> reconstruirLinhaTempo(String caminhoArquivoCsv, String sessionId) throws IOException {
+        var logs = ler(caminhoArquivoCsv);
+        var resultado = new ArrayList<String>();
 
+        for (String[] log : logs) {
+            if (sessionId.equals(log[1])) {
+                resultado.add(log[3] + " -> " + log[4]);
+            }
+        }
+        return resultado;
+    }
+    
     @Override
     public List<Alerta> priorizarAlertas(String caminhoArquivo, int n) throws IOException {
         return Collections.emptyList();
